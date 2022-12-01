5963d4889c7c5d0be557176e23b6b64bbce53df0
Artemiev Dima <adbart1985@gmail.com>
2022-12-01 21:11:04 +0300

    Begin exersice001

--- excersice001/Program.cs
+++ excersice001/Program.cs
@@ -1,2 +1,14 @@
-﻿int a = ConsoleWriteLine()
-Console.WriteLine()
+﻿/* Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт,
+какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.Write("Введите число a=");
+string numberA = Console.ReadLine();
+int a = Convert.ToInt32(numberA)
+Console.Write("Введите число b=");
+string numberB = Console.ReadLine();
+int b = Convert.ToInt32(numberB);
